ac19c30d839435c8b43715873ca0786fee789a51
Alicja Polanowska <ala.polanowska@gmail.com>
2018-06-02 18:34:35 +0200

    feature/add class book.java

---
+++ src/main/java/Book.java
@@ -0,0 +1,58 @@
+public class Book {
+    private String author;
+    private String code;
+    private String title;
+    private int pagesCount;
+    private CategoriesEnum.Categories category;
+
+    public Book() {
+    }
+
+    public Book(String author, String code, String title, int pagesCount, CategoriesEnum.Categories category) {
+        this.author = author;
+        this.code = code;
+        this.title = title;
+        this.pagesCount = pagesCount;
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPagesCount() {
+        return pagesCount;
+    }
+
+    public void setPagesCount(int pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public CategoriesEnum.Categories getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoriesEnum.Categories category) {
+        this.category = category;
+    }
+}
